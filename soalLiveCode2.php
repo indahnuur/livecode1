356199252792329d32941073221d72aaceb67a7f
Indah Nuur Hasanah <54131090+indahnuur@users.noreply.github.com>
2019-08-26 17:12:34 +0700

    live code1

---
+++ soalLiveCode2.php
@@ -0,0 +1,62 @@
+/**
+---------------------
+First Letter Grouping
+---------------------
+ 
+Diberikan sebuah variabel `name`. Buatlah sebuah script untuk menampilkan nama tersebut
+berada di group ke-berapa, dengan aturan sebagai berikut:
+- Jika huruf pertama dari `name` adalah a, b, c, d maka tampilkan 'Masuk group pertama'
+- Jika huruf pertama dari `name` adalah e, f, g, h maka tampilkan 'Masuk group kedua'
+- Selain itu tampilkan 'Masuk group terakhir'
+
+Contoh:
+- `name` = 'arief', output: 'Masuk group pertama'
+- `name` = 'rinnina', output: 'Masuk group kedua'
+- `name` = 'joshua', output: 'Masuk group terakhir'
+- `name` = 'yusril', output: 'Masuk group terakhir'
+
+*/
+
+$name = ''; // Isi nama disini
+
+// Code disini
+
+
+
+
+
+<?php
+$hurufpertama: "a,b,c,d" = $grouppertama
+$hurufpertama: "e,f,g,h," = $groupkedua
+$hurufpertama: "i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z" =$groupterakhir
+
+$name= "arief" ($grouppertama);
+$name= "rinnina","joshua","yusril"($groupterakhir);
+
+
+function $name (){
+    echo "arief";"\n";
+tampil()//Masuk group pertama
+
+
+function $name (){
+    echo "rinnina";"\n";
+tampil()//Masuk group terakhir
+
+
+function $joshua (){
+    echo "arief";"\n"
+tampil()//Masuk group terakhir
+
+
+function $name (){
+    echo "yusril";"\n";
+tampil()//Masuk group terakhir
+
+
+
+?>
+
+
+
+
